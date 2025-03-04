34c467c11685b9078a724c63e7fca88ac5ebdae5
Marcin Piesio <marcin.piesio@intercity.pl>
2025-03-04 10:22:17 +0100

    dodano sumy

--- lista.py
+++ lista.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*- 
+shopping_list = {
+    "Piekarnia": ["chleb", "pączek", "bułki"],
+    "Warzywniak": ["marchew", "seler", "rukola"]
+}
+
+print("Lista zakupów")
+for shop, items in shopping_list.items():
+    formatted_shop = shop.capitalize()
+    formatted_items = [item.capitalize() for item in items]
+    print(f"Idę do {formatted_shop}, kupuję tu następujące rzeczy: {formatted_items}.")
+    total_items = sum(len(items) for items in shopping_list.values())
+    print(f"W sumie kupuję {total_items} produktów.")
