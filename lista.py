9bbb2cd77dc653937fb55192b435767300b75aaa
Marcin Piesio <marcinpiesio91@gmail.com>
2025-03-28 12:28:21 +0100

    dodany misio

--- lista.py
+++ lista.py
@@ -11,3 +11,5 @@ for shop, items in shopping_list.items():
     print(f"Idę do {formatted_shop}, kupuję tu następujące rzeczy: {formatted_items}.")
     total_items = sum(len(items) for items in shopping_list.values())
     print(f"W sumie kupuję {total_items} produktów.")
+    total_items = sum(len(items) for items in shopping_list.values())
+print(f"W sumie kupuję {total_items} produktów.")
